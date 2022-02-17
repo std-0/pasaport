03c75944565e4e29c7313b2b12edc20f9846006f
DESKTOP-CV05TSE\casch <caschismnt@gmail.com>
2022-02-17 17:04:01 +0200

    no message

--- about.php
+++ about.php
@@ -521,87 +521,9 @@ head('Pagina principala');
     <!--====== Testimonials End ======-->
 
     <!--====== Footer area start ======-->
-    <footer>
-        <div class="footer-widgets">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-4 col-md-6">
-                        <div class="widget text-block-widget">
-                            <h4 class="widget-title">About Us.</h4>
-                            <p>
-                                Lorem ipsum dolor sit amet, consectetur adi
-                                pisicing elit, sed do eiusmod tempor incidid
-                                unt ut labore et dolore magna aliqua. Ut en
-                                im ad minim veniam, quis nostrud exercit
-                                ation ullamco laboris nisi ut aliquip ex ea
-                                commodo consequat.
-                            </p>
-                            <a href="about.html#" class="read-more-btn">Read More</a>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-6 col-sm-6">
-                        <div class="widget menu-widget">
-                            <h4 class="widget-title">Our Pages.</h4>
-                            <ul>
-                                <li><a href="about.html">About Us</a></li>
-                                <li><a href="blog.html">Latest Post</a></li>
-                                <li><a href="about.html#">Selling Tips</a></li>
-                                <li><a href="about.html#">Advertising</a></li>
-                                <li><a href="about.html#">Contact Us</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-6 col-sm-6">
-                        <div class="widget menu-widget">
-                            <h4 class="widget-title">services.</h4>
-                            <ul>
-                                <li><a href="service-details.html">web development</a></li>
-                                <li><a href="service-details.html">ui/ux design</a></li>
-                                <li><a href="service-details.html">app development</a></li>
-                                <li><a href="service-details.html">game development</a></li>
-                                <li><a href="service-details.html">consultancy</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 offset-lg-1">
-                        <div class="widget newsletter-widget">
-                            <h4 class="widget-title">Newsletter.</h4>
-                            <form action="about.html#">
-                                <div class="input-group">
-                                    <input type="text" placeholder="Enter full name">
-                                    <span class="icon"><i class="fal fa-user"></i></span>
-                                </div>
-                                <div class="input-group">
-                                    <input type="email" placeholder="Enter email address">
-                                    <span class="icon"><i class="fal fa-envelope"></i></span>
-                                </div>
-                                <div class="input-group">
-                                    <button type="submit" class="main-btn"><span
-                                            class="icon-left fal fa-paper-plane"></span>Submit Now</button>
-                                </div>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="footer-bottom">
-            <div class="container">
-                <div class="row align-items-center justify-content-between">
-                    <div class="col-md-6 col-12">
-                        <div class="footer-logo text-center text-md-left">
-                            <img src="assets/img/logo.png" alt="Yankee">
-                        </div>
-                    </div>
-                    <div class="col-md-6 col-12">
-                        <div class="copyright text-center text-md-right">
-                            <p>Copyright ©2020. All Rights Reserved</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
+    <?php
+	footer();
+	?>
     <!--====== Footer area end ======-->
 
 	<!--====== jquery js ======-->
