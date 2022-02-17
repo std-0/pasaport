5a4ed4669f8a0114c4b47c0726b27dc982ad80de
DESKTOP-CV05TSE\casch <caschismnt@gmail.com>
2022-02-17 17:51:52 +0200

    no message

--- contact.php
+++ contact.php
@@ -4,25 +4,25 @@ head("pagina principala")
 ?>
 
     <!--====== Breadcrumb Start ======-->
-    <section class="breadcrumb-section" style="background-image: url(assets/img/breadcrumb.jpg);">
+    <section class="breadcrumb-section" style="background-image: url(assets/img/banner/baner01.jpg);">
         <div class="container">
-            <span class="page-tag">Welcome to our company</span>
-            <h1 class="page-title">contact us</h1>
+            <span class="page-tag" style="font-style:capitalize">румынские паспорты</span>
+            <h1 class="page-title" style="font-size:80px;">СВЯЗАТЬСЯ С НАМИ</h1>
 
-            <ul class="breadcrumb-nav">
+            <!-- <ul class="breadcrumb-nav">
                 <li><a href="contact.html#">Home</a></li>
                 <li>contact us</li>
-            </ul>
+            </ul> -->
         </div>
     </section>
     <!--====== Breadcrumb end ======-->
 
     <!--====== Contact Maps Start ======-->
-    <section class="contact-maps">
+    <!-- <section class="contact-maps">
         <iframe
             src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d16249.383697457268!2d91.8743084038307!3d24.893251768165847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1603347568911!5m2!1sen!2sbd"
             allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
-    </section>
+    </section> -->
     <!--====== Contact Maps End ======-->
 
     <!--====== Contact Section Start ======-->
@@ -32,21 +32,21 @@ head("pagina principala")
                 <div class="col-lg-4 col-md-10">
                     <div class="contact-info">
                         <div class="section-title mb-40">
-                            <span class="title-tag">Contact info</span>
-                            <h2 class="title">catch us here</h2>
+                            <span class="title-tag">Контактная информация</span>
+                            <h2 class="title">поместите данные здесь</h2>
                         </div>
                         <ul class="contact-info-list">
-                            <li>
+                            <!-- <li>
                                 <p>
                                     2005 Stokes Isle Apt. 896, Venaville 10010, United States
                                 </p>
                                 <a href="contact.html#">Get Directions</a>
-                            </li>
+                            </li> -->
                             <li>
                                 <p>
                                     +987-786-765-765-87 <br> +9 (877) 86-765-765-87
                                 </p>
-                                <a href="contact.html#">make call</a>
+                                <a href="contact.html#">вызов</a>
                             </li>
                         </ul>
                     </div>
@@ -56,27 +56,17 @@ head("pagina principala")
                         <form action="contact.html#">
                             <div class="row">
                                 <div class="col-lg-6 mb-20">
-                                    <input type="text" placeholder="First name" id="FirstName" name="FirstName">
+                                    <input type="text" placeholder="полное имя" id="FirstName" name="FirstName">
                                 </div>
                                 <div class="col-lg-6 mb-20">
-                                    <input type="text" placeholder="Last name" id="LastName" name="LastName">
+                                    <input type="text" placeholder="телефонный номер" id="LastName" name="LastName">
                                 </div>
-                                <div class="col-lg-6 mb-20">
-                                    <input type="email" placeholder="Email address" id="email" name="email">
-                                </div>
-                                <div class="col-lg-6 mb-20">
-                                    <select name="subject" id="subject">
-                                        <option disabled selected>Subject</option>
-                                        <option value="Subject One">Subject One</option>
-                                        <option value="Subject Two">Subject Two</option>
-                                        <option value="Subject Three">Subject Two</option>
-                                    </select>
-                                </div>
+                             
                                 <div class="col-12 mb-20">
-                                    <textarea id="message" name="message"></textarea>
+                                    <textarea id="message" name="message" placeholder="Что вас интересует"></textarea>
                                 </div>
                                 <div class="col-12 text-center">
-                                    <button type="submit" class="main-btn main-btn-2">Get A Quote</button>
+                                    <button type="submit" class="main-btn main-btn-2">получить консультацию</button>
                                 </div>
                             </div>
                         </form>
