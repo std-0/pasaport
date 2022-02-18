fd20d368b3b9b48173f545116bc56703891ddaed
DESKTOP-CV05TSE\casch <caschismnt@gmail.com>
2022-02-18 13:31:31 +0200

    no message

--- Function/benefits.php
+++ Function/benefits.php
@@ -8,8 +8,9 @@ function benefits()
 <section class="latest-blog-section section-gap-less-bottom">
         <div class="container">
             <div class="section-title text-center mb-60">
-                <span class="title-tag">News & Blog</span>
-                <h2 class="title">Update news & Blog</h2>
+                <!-- <span class="title-tag">News & Blog</span> -->
+                <h2 class="title">ЧТО МОЖНО ПОЛУЧИТЬ</h2>
+                <h2 class="title">С ГРАЖДАНСТВОМ РУМЫНИИ</h2>
             </div>
             <div class="row latest-blog-posts justify-content-center">
                 <div class="col-lg-4 col-md-6 col-sm-11">
@@ -18,9 +19,9 @@ function benefits()
 						<div class="blog-desc">
                            
 							<h3 class="title">
-								<a href="index-5-dark.html#">Overall Usability and Functionality of Website.</a>
+								<a href="index-5-dark.html#">Право работать на всей территории ЕС и получать достойную зарплату</a>
 							</h3>
-                            <a href="index-5-dark.html#" class="read-more-btn"><i class="far fa-arrow-right"></i>Read More</a>
+                            
 						</div>
 					</div>
                 </div>
@@ -30,9 +31,9 @@ function benefits()
 						<div class="blog-desc">
                            
 							<h3 class="title">
-								<a href="index-5-dark.html#">Overall Usability and Functionality of Website.</a>
+								<a href="index-5-dark.html#">Возможность посещать более 170 стран мира в безвизовом режиме </a>
 							</h3>
-                            <a href="index-5-dark.html#" class="read-more-btn"><i class="far fa-arrow-right"></i>Read More</a>
+                            
 						</div>
 					</div>
                 </div>
@@ -42,9 +43,9 @@ function benefits()
 						<div class="blog-desc">
                            
 							<h3 class="title">
-								<a href="index-5-dark.html#">Overall Usability and Functionality of Website.</a>
+								<a href="index-5-dark.html#">Право на бесплатное обучение в лучших европейских ВУЗах</a>
 							</h3>
-                            <a href="index-5-dark.html#" class="read-more-btn"><i class="far fa-arrow-right"></i>Read More</a>
+                            
 						</div>
 					</div>
                 </div>
@@ -56,9 +57,9 @@ function benefits()
 						<div class="blog-desc">
                            
 							<h3 class="title">
-								<a href="index-5-dark.html#">Overall Usability and Functionality of Website.</a>
+								<a href="index-5-dark.html#">Возможность неограниченного проживания в любой стране ЕС.</a>
 							</h3>
-                            <a href="index-5-dark.html#" class="read-more-btn"><i class="far fa-arrow-right"></i>Read More</a>
+                            
 						</div>
 					</div>
                 </div>
@@ -68,9 +69,9 @@ function benefits()
 						<div class="blog-desc">
                            
 							<h3 class="title">
-								<a href="index-5-dark.html#">Overall Usability and Functionality of Website.</a>
+								<a href="index-5-dark.html#">Право на социальную защиту и высокое качество жизни</a>
 							</h3>
-                            <a href="index-5-dark.html#" class="read-more-btn"><i class="far fa-arrow-right"></i>Read More</a>
+                            
 						</div>
 					</div>
                 </div>
@@ -80,52 +81,15 @@ function benefits()
 						<div class="blog-desc">
                            
 							<h3 class="title">
-								<a href="index-5-dark.html#">Overall Usability and Functionality of Website.</a>
+								<a href="index-5-dark.html#">Возможность получать медицинскую помощь на территории ЕС</a>
 							</h3>
-                            <a href="index-5-dark.html#" class="read-more-btn"><i class="far fa-arrow-right"></i>Read More</a>
+                            
 						</div>
 					</div>
                 </div>
             </div>
 
-            <div class="row latest-blog-posts justify-content-center">
-                <div class="col-lg-4 col-md-6 col-sm-11">
-                    <div class="latest-blog-post-two">
-						
-						<div class="blog-desc">
-                           
-							<h3 class="title">
-								<a href="index-5-dark.html#">Overall Usability and Functionality of Website.</a>
-							</h3>
-                            <a href="index-5-dark.html#" class="read-more-btn"><i class="far fa-arrow-right"></i>Read More</a>
-						</div>
-					</div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-11">
-                    <div class="latest-blog-post-two">
-						
-						<div class="blog-desc">
-                           
-							<h3 class="title">
-								<a href="index-5-dark.html#">Overall Usability and Functionality of Website.</a>
-							</h3>
-                            <a href="index-5-dark.html#" class="read-more-btn"><i class="far fa-arrow-right"></i>Read More</a>
-						</div>
-					</div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-11">
-                    <div class="latest-blog-post-two">
-						
-						<div class="blog-desc">
-                           
-							<h3 class="title">
-								<a href="index-5-dark.html#">Overall Usability and Functionality of Website.</a>
-							</h3>
-                            <a href="index-5-dark.html#" class="read-more-btn"><i class="far fa-arrow-right"></i>Read More</a>
-						</div>
-					</div>
-                </div>
-            </div>
+            
         </div>
     </section>
 
